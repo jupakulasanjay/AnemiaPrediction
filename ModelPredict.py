99e7a19d71833b6886aa7fd20ebf3ed94de988a9
Sanjay Jupakula <sanjay.jupakula@springworks.in>
2024-04-14 23:59:46 +0530

    UPDATE: Accurate DNN_model implementation

--- ModelPredict.py
+++ ModelPredict.py
@@ -1,67 +1,171 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import tensorflow as tf
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-import joblib
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
 
-def save_model(model, model_filename):
-    joblib.dump(model, model_filename)
+data = pd.read_csv(r"./uploads/uploaded_file.csv")
+df = pd.DataFrame(data)
 
-def load_model(model_filename):
-    return joblib.load(model_filename)
 
-def predict_Dlresult(user_input):
+X = df.drop(columns=['All_Class', 'HGB_Anemia_Class',	'Iron_anemia_Class', 'Folate_anemia_class', 'B12_Anemia_class'])
 
-    print(user_input)
-    filename = "./uploads/uploaded_file.csv"
-    df = pd.read_csv(filename)
-    feature_names = [
-        'GENDER', 'RBC', 'HGB', 'HCT', 'MCV', 'MCH', 'MCHC', 'RDW', 'FOLATE', 'B12'
-    ]
-    
-    user_data = pd.DataFrame(user_input, index=[0])
-    
-    X_train = df[feature_names]
-    y_train = df['All_Class']
-    
-    # Modify GENDER feature
-    user_data['GENDER'] = 1 if user_input['GENDER'] == 'Male' else 0
-    
-    X_test = user_data[feature_names]
-    
-    rf_model = load_model('./rf_model.joblib')
-    gb_model = load_model('./gb_model.joblib')
-    dnn_model = tf.keras.models.load_model('./dnn_model1.h5')
-    
-    X_train_base, X_test_base, y_train_base, y_test_base = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
-    
-    predictions_rf = rf_model.predict(X_test)
-    predictions_gb = gb_model.predict(X_test)
-    # predictions_dnn = dnn_model.predict(X_test)
-    predictions_dnn = dnn_model.predict(X_test)
-    dnn_predictions = np.round(predictions_dnn).astype(int)
-    
-    accuracy_rf = accuracy_score(y_test_base, rf_model.predict(X_test_base))
-    accuracy_gb = accuracy_score(y_test_base, gb_model.predict(X_test_base))
+y = df['All_Class']
 
-    print(accuracy_rf, accuracy_gb)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 39)
 
-    return int(predictions_rf[0]), int(predictions_gb[0]), int(dnn_predictions[0][0])
+print("kchjvgxckv",X_train, X_test, y_train, y_test )
 
-# # Example usage:
-# user_input = {
-#     'GENDER': 'Male',
-#     'RBC': 4.31,
-#     'HGB': 12.7,
-#     'HCT': 37.6,
-#     'MCV': 87.2,
-#     'MCH': 29.5,
-#     'MCHC': 33.8,
-#     'RDW': 12.8,
-#     'FOLATE': 5.06,
-#     'B12': 178.2
-# }
+scalar = StandardScaler()
+X_trained_scaled = scalar.fit_transform(X_train)
+X_test_scaled = scalar.transform(X_test)
 
-# predict_Dlresult(user_input)
+label = LabelEncoder()
+y_train_enc = label.fit_transform(y_train)
+y_test_enc = label.transform(y_test)
+
+print(y_train,"\ndsregf.jkdsjghdsfl", y_train_enc)
+
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(128, activation='relu', input_shape=(X_train.shape[1],)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(len(np.unique(y_train)), activation='softmax')
+])
+
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+prd = model.fit(X_trained_scaled, y_train_enc, epochs = 50, batch_size = 16, validation_split = 0.2)
+
+test_loss, test_accuracy = model.evaluate(X_test_scaled, y_test_enc)
+print(f"Test ACCuracy: {test_accuracy * 100}")
+
+predictions = model.predict(X_test_scaled)
+
+predicted_labels = np.argmax(predictions, axis = 1)
+
+predicted_classes = label.inverse_transform(predicted_labels)
+
+# testing = pd.DataFrame({
+#     'GENDER': [1],
+#     'RBC': [5.57],
+#     'HGB': [15.87],
+#     'HCT': [47.43],
+#     'MCV': [85.14],
+#     'MCH': [28.48],
+#     'MCHC': [33.45],
+#     'RDW': [11.57],
+#     'FOLATE': [6.43],
+#     'B12': [162.5],
+# })
+
+# testing = pd.DataFrame({
+#     'GENDER': [0],
+#     'RBC': [4.16],
+#     'HGB': [11.8],
+#     'HCT': [35.1],
+#     'MCV': [84.4],
+#     'MCH': [28.4],
+#     'MCHC': [33.6],
+#     'RDW': [12.9],
+#     'FOLATE': [10.02],
+#     'B12': [675.9],
+# })
+
+
+# testing = pd.DataFrame({
+#     'GENDER': [0],
+#     'RBC': [3.65],
+#     'HGB': [8.18],
+#     'HCT': [26.25],
+#     'MCV': [71.83],
+#     'MCH': [22.39],
+#     'MCHC': [31.18],
+#     'RDW': [18.5],
+#     'FOLATE': [8.37],
+#     'B12': [157.5],
+# })
+
+
+testing = pd.DataFrame({
+    'GENDER': [1],
+    'RBC': [4.6],
+    'HGB': [12.1],
+    'HCT': [37.6],
+    'MCV': [81.7],
+    'MCH': [26.3],
+    'MCHC': [32.2],
+    'RDW': [15.4],
+    'FOLATE': [3.51],
+    'B12': [217.6],
+})
+
+# testing = pd.DataFrame({
+#     'GENDER': [1],
+#     'RBC': [4.47],
+#     'HGB': [12.9],
+#     'HCT': [38.5],
+#     'MCV': [86.2],
+#     'MCH': [29],
+#     'MCHC': [33.6],
+#     'RDW': [16.6],
+#     'FOLATE': [6.2],
+#     'B12': [287.2],
+# })
+
+# testing = pd.DataFrame({
+#     'GENDER': [1],
+#     'RBC': [3.1],
+#     'HGB': [13.2],
+#     'HCT': [38.3],
+#     'MCV': [123.5],
+#     'MCH': [42.6],
+#     'MCHC': [34.5],
+#     'RDW': [12.2],
+#     'FOLATE': [15.2],
+#     'B12': [247.5],
+# })
+
+# testing = pd.DataFrame({
+#     'GENDER': 1,
+#     'RBC': 3.1,
+#     'HGB': 13.2,
+#     'HCT': 38.3,
+#     'MCV': 123.5,
+#     'MCH': 42.6,
+#     'MCHC': 34.5,
+#     'RDW': 12.2,
+#     'FOLATE': 15.2,
+#     'B12': 247.5,
+# })
+
+
+
+
+# Scale the input data using the same scalar used for training data
+testing_scaled = scalar.transform(testing)
+
+# Make predictions on the scaled input data
+predictions = model.predict(testing_scaled)
+
+# Convert predictions to class labels
+predicted_labels = np.argmax(predictions, axis=1)
+predicted_classes = label.inverse_transform(predicted_labels)
+
+print(predictions,predicted_labels, predicted_classes)
+
+
+predicted_anemia = 0
+if ( predicted_classes[0] == 0) : 
+    predicted_anemia = 0    
+else: 
+    predicted_anemia = 1
+    
+print(predicted_anemia)
+
--- app1.py
+++ app1.py
@@ -6,17 +6,30 @@ from sklearn.model_selection import train_test_split
 from flask import Flask, render_template, request
 from sklearn.metrics import accuracy_score
 import joblib
-# from ModelPredict import predict_Dlresult
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
 
 app = Flask(__name__, static_url_path='/static')
 
 def load_model(model_filename):
     return joblib.load(model_filename)
 
-# # Load saved models
-# rf_model = load_model('rf_model.pkl')
-# gb_model = load_model('gb_model.pkl')
-# dnn_model = tf.keras.models.load_model('dnn_model.h5')
+data = pd.read_csv(r"./uploads/uploaded_file.csv")
+df = pd.DataFrame(data)
+
+X = df.drop(columns=['All_Class', 'HGB_Anemia_Class', 'Iron_anemia_Class', 'Folate_anemia_class', 'B12_Anemia_class'])
+
+y = df['All_Class']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 39)
+
+scalar = StandardScaler()
+X_trained_scaled = scalar.fit_transform(X_train)
+X_test_scaled = scalar.transform(X_test)
+
+label = LabelEncoder()
+y_train_enc = label.fit_transform(y_train)
+y_test_enc = label.transform(y_test)
 
 @app.route('/contactUs')
 def contactUs():
@@ -39,25 +52,19 @@ def predict():
     ]
 
     user_input = {feature: request.form.get(feature) for feature in feature_names}
-    # user_data = pd.DataFrame(user_input, index=[0])
 
     for i in user_input:
         if i != 'GENDER':
             user_input[i] = float(user_input[i])
 
-    print(user_input)
-
     predictions_rf, predictions_gb, dnn_predictions = predict_Dlresult(user_input)
-    print(predictions_rf, predictions_gb, dnn_predictions)
     
     combined_predictions = (predictions_rf + predictions_gb + dnn_predictions) / 3.0
     predicted_anemia = np.round(combined_predictions)
     predicted_class = (predictions_rf)
-    return render_template('result.html', predicted_anemia=predicted_anemia, predicted_class=predicted_class)
+    return render_template('result.html', predicted_anemia=predicted_anemia, predicted_class=predicted_class, predicted_anemia_dl=dnn_predictions)
 
-def predict_Dlresult(user_input):
-
-    print(user_input)
+def predict_Dlresult(user_input):    
     filename = "./uploads/uploaded_file.csv"
     df = pd.read_csv(filename)
     feature_names = [
@@ -71,27 +78,48 @@ def predict_Dlresult(user_input):
     
     # Modify GENDER feature
     user_data['GENDER'] = 1 if user_input['GENDER'] == 'Male' else 0
+
+    user_data_dnn = {
+        'GENDER': [user_input['GENDER']],
+        'RBC': [user_input['RBC']],
+        'HGB': [user_input['HGB']],
+        'HCT': [user_input['HCT']],
+        'MCV': [user_input['MCV']],
+        'MCH': [user_input['MCH']],
+        'MCHC': [user_input['MCHC']],
+        'RDW': [user_input['RDW']],
+        'FOLATE': [user_input['FOLATE']],
+        'B12': [user_input['B12']]
+    }
+
+    user_data_dnn['GENDER'] = [1] if user_input['GENDER'] == 'Male' else [0]
+
+    user_data_dnn = pd.DataFrame(user_data_dnn, index=[0])
+
+    testing_scaled = scalar.transform(user_data_dnn)
     
     X_test = user_data[feature_names]
+    X_test_dnn = user_data_dnn[feature_names]
     
     rf_model = load_model('./rf_model.joblib')
     gb_model = load_model('./gb_model.joblib')
-    dnn_model = tf.keras.models.load_model('./dnn_model1.h5')
+    dnn_model = tf.keras.models.load_model('./dnn_model3.h5')
     
     X_train_base, X_test_base, y_train_base, y_test_base = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
     
     predictions_rf = rf_model.predict(X_test)
     predictions_gb = gb_model.predict(X_test)
-    # predictions_dnn = dnn_model.predict(X_test)
-    predictions_dnn = dnn_model.predict(X_test)
+
+    predictions_dnn = dnn_model.predict(testing_scaled)
+    predicted_labels = np.argmax(predictions_dnn, axis=1)
+
     dnn_predictions = np.round(predictions_dnn).astype(int)
+    predicted_labels = np.argmax(dnn_predictions, axis=1)
     
     accuracy_rf = accuracy_score(y_test_base, rf_model.predict(X_test_base))
     accuracy_gb = accuracy_score(y_test_base, gb_model.predict(X_test_base))
 
-    print(accuracy_rf, accuracy_gb)
-
-    return int(predictions_rf[0]), int(predictions_gb[0]), int(dnn_predictions[0][0])
+    return int(predictions_rf[0]), int(predictions_gb[0]), int(predicted_labels[0])
 
 if __name__ == "__main__":
     app.run(debug=True)
